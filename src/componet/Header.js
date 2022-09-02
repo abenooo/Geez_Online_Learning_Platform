a6242cd3c118774e061902c60880c58e4740943c
aR <abenezerkifle000@gmail.com>
2022-09-02 20:31:03 +0300

    Finesh header email and phone

--- src/componet/Header.js
+++ src/componet/Header.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import './header.css';
+import React from "react";
+import "./header.css";
 // import { BiChevronRight } from "react-icons/bi";
 function Header() {
   return (
-    <div className='header'>
-        <div className="header_left">
-{/* <BiChevronRight /> */}
-<p className='email'>Email Adddres</p>
-<p className='phone'>phone addres</p>
+    <div className="header">
+      <div className="header_left">
+        {/* <BiChevronRight /> */}
+        <div>
+          <p className="language">EN</p>
         </div>
-        <div className="header_right">
+        <div>
+          <p className="email">Email Adddres</p>
+        </div>
+        <div>
+          <p className="phone">Phone</p>
+        </div>
+      </div>
 
-        </div>
+      <div className="header_right"></div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
