89c485bb5f28597a16c46f813c0c72166ecde241
aR <abenezerkifle000@gmail.com>
2022-09-02 18:49:25 +0300

    Add icons

--- src/componet/Header.js
+++ src/componet/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import './header.css'
+import './header.css';
+// import { BiChevronRight } from "react-icons/bi";
 function Header() {
   return (
     <div className='header'>
         <div className="header_left">
-
+{/* <BiChevronRight /> */}
+<p className='email'>Email Adddres</p>
+<p className='phone'>phone addres</p>
         </div>
         <div className="header_right">
-            
+
         </div>
     </div>
   )
